db72537f3d939a474fe7a6fc374755251f93f61c
David Chiedu <dedestunting@gmail.com>
2025-01-07 02:47:51 -0800

    Addred Init Store

---
+++ src/store/Store.ts
@@ -0,0 +1,34 @@
+
+export type UserId = string;
+
+export interface Chat {
+    id: string;
+    userId: UserId;
+    name: string;
+    message: string;
+    upvotes: UserId[]; //who has upvoted what
+}
+export  abstract class Store {
+    constructor() {
+
+    }
+    initRoom(roomId: string) {
+
+    }
+
+    getChats(oom: string, limit: number, offset: number) {
+
+    }
+    getRoom(room: string, limit: number, offset: number) {
+
+    }
+
+    addChat(userId: UserId, name: string, room: string, message:string) {
+
+    }
+
+    upVote(userId: UserId, room: string, chatId: string) {
+
+    }
+
+}
